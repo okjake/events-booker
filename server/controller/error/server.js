f8fb8afffb0515462ce72aae54ed4b419433d344
MohammedIrheem <56350350+MohammedIrheem@users.noreply.github.com>
2020-03-26 20:26:33 +0200

    Update server.js

--- server/controller/error/server.js
+++ server/controller/error/server.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-unused-vars
 exports.serverError = (err, req, res, next) => {
-  res.status(err.status || 500).json({ msg: err.msg, status: err.status });
+  res.status(err.status || 500).json({ msg: err.msg});
 };
