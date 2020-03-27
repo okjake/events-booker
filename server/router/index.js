c06da79de759d45fec3c3cb5a92f41e895a9eb54
AhmedSafi97 <ahmadhsafi1997@gmail.com>
2020-03-27 15:56:31 +0300

    create pull request relates #19

--- server/router/index.js
+++ server/router/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const { serverError, clientError } = require('../controller');
 
-
+router.post('/checkUser', (req, res) => res.json(req.body));
 router.use(clientError);
 router.use(serverError);
 
