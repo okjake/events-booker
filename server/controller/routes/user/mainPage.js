01904eeac2544760b99b564fd75524413a247e7a
rawan <rawan.designer@hotmail.com>
2020-03-29 00:43:56 +0300

    Error Handling Relates #11

--- server/controller/routes/user/mainPage.js
+++ server/controller/routes/user/mainPage.js
@@ -1,11 +1,18 @@
 const getAllEvents = require("../../../database/queries/getEvents");
 
-const getEvents = (req, res) => {
+const getEvents = (req, res, next) => {
   getAllEvents()
-    .then(({ rows }) => res.json(rows))
-    .catch(() => {
-      res.status(400).json({ error: "Page Not Found" });
-    });
+    .then(({ rows }) => {
+      if (rows.length) {
+        res.json(rows);
+      } else {
+        const error = new Error();
+        error.msg = "This page is not found";
+        error.status = 400;
+        next(error);
+      }
+    })
+    .catch(next);
 };
 
 module.exports = { getEvents };
