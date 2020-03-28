cdf03370851ce0dbe7c3e4a6342113a99c3265e9
rawan <rawan.designer@hotmail.com>
2020-03-28 23:56:08 +0300

    Editing the query of the the get events Relates #11

--- server/database/queries/getEvents.js
+++ server/database/queries/getEvents.js
@@ -1,5 +1,13 @@
 const connection = require("../config/connection");
 
-const getAllEvents = () => connection.query("SELECT * FROM events");
+const getAllEvents = () => connection.query
+("SELECT  events.title, events.category, events.event_code, events.details, events.image, events.date, events.duration, COUNT(user_event.event_id) 
+AS Count 
+FROM events 
+LEFT JOIN user_event
+ON (user_event.event_id = events.id)
+WHERE events.expired = false
+GROUP BY event_id,  events.title, events.category, events.event_code, events.details, events.image, events.date, events.duration;
+);
 
 module.exports = getAllEvents;
