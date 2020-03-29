bb27db9aa88e2741cd36f8c4ed6bc0ff0ba38687
mariamisa <maryaam1998@hotmail.com>
2020-03-29 23:06:39 +0300

    change var name relates #33

--- server/database/config/admin.sql
+++ server/database/config/admin.sql
@@ -3,6 +3,7 @@ BEGIN;
 INSERT INTO
     admin(name, email, password)
 VALUES
-    ('mariam','mariam@gmail.com', 22446688),
+    ('mariam','mariam@gmail.com','$2y$12$PrSt10v/N1cSo8ea5uVDoOtQFOqhYz7dsW5IdwC3cihnmaF2TQ5MG
+'),
     ('moh','moh@gmail.com', '$2y$12$1edtV4qo8pzIjL1/o0Nhs.c/Tlrm5b2sFF/IyKpXa.vvFZi0Gte.y');
 COMMIT;
