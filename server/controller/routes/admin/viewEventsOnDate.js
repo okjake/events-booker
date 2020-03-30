cbcb841bb1605c7f5b6297d3ebcfcd15e697d789
mariamisa <maryaam1998@hotmail.com>
2020-03-31 02:04:29 +0300

    replace slice with moment format relates #53

--- server/controller/routes/admin/viewEventsOnDate.js
+++ server/controller/routes/admin/viewEventsOnDate.js
@@ -6,7 +6,7 @@ today.format('YYYY-MM-DD');
 const viewEventsOnDate = (req, res, next) => {
   getEventsDate().then(({ rows }) => {
     if (rows.length !== 0) {
-      const events = rows.filter((el) => el.date.slice(0, 10) === today);
+      const events = rows.filter((el) => moment(el.date).format('YYYY-MM-DD') === today);
       res.json(events);
     } else {
       res.json('no events on this day');
