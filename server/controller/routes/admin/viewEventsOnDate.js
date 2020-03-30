bc3eed3586e941d9cffa8e2576fdd791bc87f588
mariamisa <maryaam1998@hotmail.com>
2020-03-30 22:47:31 +0300

    fix catch on view event controller relates #53

--- server/controller/routes/admin/viewEventsOnDate.js
+++ server/controller/routes/admin/viewEventsOnDate.js
@@ -1,13 +1,13 @@
 const { getEventsDate } = require('../../../database/queries/getEventsWithDate');
 
-const viewEventsOnDate = (req, res) => {
+const viewEventsOnDate = (req, res, next) => {
   getEventsDate().then(({ rows }) => {
     if (rows.length !== 0) {
       res.json(rows);
     } else {
       res.json('no events on this day');
     }
-  });
+  }).catch(next);
 };
 
 module.exports = { viewEventsOnDate };
