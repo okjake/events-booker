f98c03a664cd288cc22c4575e458e3816b404b80
rawan <rawan.designer@hotmail.com>
2020-03-30 11:54:32 +0300

    Editing the query which give me the users of the events, so I added the attendance of the user to the data Relates #38

--- server/database/queries/getUsersOfEvent.js
+++ server/database/queries/getUsersOfEvent.js
@@ -1,5 +1,5 @@
 const connection = require('../config/connection');
 
-const getUsersOfEvent = (eventCode) => connection.query('SELECT users.first_name, users.last_name, users.mobile, users.email,users.location FROM users INNER JOIN user_event ON users.id = user_event.user_id INNER JOIN events ON user_event.event_id = events.id WHERE events.event_code = 1$ ', [eventCode]);
+const getUsersOfEvent = (eventCode) => connection.query('SELECT users.first_name, users.last_name, users.mobile, users.email,users.location, user_event.attendance FROM users INNER JOIN user_event ON users.id = user_event.user_id INNER JOIN events ON user_event.event_id = events.id WHERE events.event_code = 1$ ', [eventCode]);
 
 module.exports = getUsersOfEvent;
