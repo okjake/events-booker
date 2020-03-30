44cf7a3571851a9604fe72b15db9f08a9a275e58
mariamisa <maryaam1998@hotmail.com>
2020-03-31 01:42:56 +0300

    update get events with date relates #53

--- server/controller/routes/admin/viewEventsOnDate.js
+++ server/controller/routes/admin/viewEventsOnDate.js
@@ -1,9 +1,13 @@
+const moment = require('moment');
 const { getEventsDate } = require('../../../database/queries/getEventsWithDate');
 
+const today = moment();
+today.format('YYYY-MM-DD');
 const viewEventsOnDate = (req, res, next) => {
   getEventsDate().then(({ rows }) => {
     if (rows.length !== 0) {
-      res.json(rows);
+      const events = rows.filter((el) => el.date.slice(0, 10) === today);
+      res.json(events);
     } else {
       res.json('no events on this day');
     }
