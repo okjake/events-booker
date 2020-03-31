67fdd9108a4c424e17a925e7e6fabb9e7a776b7d
AlaaTaima <alaa.ktaima@gmail.com>
2020-04-01 02:08:45 +0300

    console.log

--- server/controller/error/server.js
+++ server/controller/error/server.js
@@ -1,4 +1,4 @@
 exports.serverError = (err, req, res, next) => {
-  console.log(err);
+  // console.log(err);
   res.status(err.status || 500).json({ msg: err.msg || 'Something went wrong, try again later' });
 };
