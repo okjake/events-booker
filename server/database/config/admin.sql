e54a70a1942f3d5cdd73ca9fce699dfec80e0fc0
AhmedSafi97 <ahmadhsafi1997@gmail.com>
2020-04-01 15:11:13 +0300

    add pin code to admin fake data schema

--- server/database/config/admin.sql
+++ server/database/config/admin.sql
@@ -3,6 +3,16 @@ BEGIN;
 INSERT INTO
     admin(name, email, password)
 VALUES
-    ('events booker admin','events.booker.ca@gmail.com','$2b$10$7gMjB14U1cWG9UWaZb4nV.UfRLqxIbSDkPXzceRvX0TldBq8uVOWm'),
-    ('moh','moh@gmail.com', '$2b$10$NFThDzVCJ9G1FPaJ.U7TU..DwMEiaPvkczZ6hWHePLsyrqMcicoWC');
+    (
+        'moh',
+        'moh@gmail.com',
+        '$2b$10$NFThDzVCJ9G1FPaJ.U7TU..DwMEiaPvkczZ6hWHePLsyrqMcicoWC',
+        '$2b$10$oMMY8FbR7QH5vRWLa.a4TO18kGaDTj4hZt2pSdUPl2t.ByfZkoyO6'
+    );
+
 COMMIT;
+
+/*
+ password = 12345678
+ pincode = 12345678
+ */
