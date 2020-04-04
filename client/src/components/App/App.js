efad8c281e62d3a5e2223d8d228e47f0a15c3b43
AlaaTaima <alaa.ktaima@gmail.com>
2020-04-04 15:36:06 +0300

    add react router

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter,Route, Switch } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Events Booker</h1>
+      <BrowserRouter>
+      <Switch>
+        
+      </Switch>
+      </BrowserRouter>
     </div>
   );
 }
