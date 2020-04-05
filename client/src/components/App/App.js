4bd85e70d68bc6f200fac243476edb8dd3e25301
rawan <rawan.designer@hotmail.com>
2020-04-05 05:19:47 +0300

    editing the card component Relates #13

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Card from '../common/Card'
 
 function App() {
   return (
     <div className="App">
       <h1>Events Booker</h1>
+      <Card />
     </div>
   );
 }
--- client/src/components/common/Card/index.js
+++ client/src/components/common/Card/index.js
@@ -1,8 +1,38 @@
 //react function component 
-import React from 'reat';
+import React from 'react';
+//import '../Card/style.css'
+import { Button } from "antd";
+//import 'antd/dist/antd.css';
 
-function Card() {
+
+
+const Card = (props)=> {
+    const {image,title,category,date} = props;
+    console.log(props);
+
+    return(
+        <div>
+
+        <img src='https://www.thebalancesmb.com/thmb/E6hp3YFsPw8mCK_39bw94CxE4Vk=/3456x3456/smart/filters:no_upscale()/asian-businesswoman-leading-meeting-at-boardroom-table-504987926-5ad21419c5542e0036d7003e.jpg' alt=""/>
+        <p>Event</p>
+        <p>coding</p>
+        <p>12/12</p>
+        
+        <div>
+        <Button type="primary">Primary</Button>
+        </div>
+    </div>
+    );
 
 }
 
 export default Card;
+
+///
+///<div>
+          //  <img src={image} alt=""/>
+            //<p>{title}</p>
+            //<p>{category}</p>
+            //<p>{date}</p>
+            //<Button type="primary">Primary</Button>
+       // </div>
