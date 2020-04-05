fc48ec67545880229c637f7ca394b680286967a8
rawan <rawan.designer@hotmail.com>
2020-04-06 00:52:25 +0300

    Adding the final touch on the card component Relates #13

--- client/src/components/common/Card/index.js
+++ client/src/components/common/Card/index.js
@@ -10,6 +10,33 @@ const Card = (props)=> {
     console.log(props);
     return(
         <div class="container">
+        <div class="card">
+
+            <div class="card__image-container">
+                <img class="card__image"  src={image} alt=""/>
+            </div>
+
+            <div class="card__content">
+                <p class='card__p'><b>Event Title :</b> {title}</p>
+                <p class='card__p'><b>Event category :</b> {category}</p>
+                <p class='card__p'><b>Event Date :</b> {date}</p>
+            </div>
+
+            <div class='card__button'>
+                <Button type="primary">See More</Button>
+            </div>
+        </div>
+    </div>
+    )
+
+}
+
+export default Card;
+
+
+
+// This code to see the final result of the card 
+/*<div class="container">
 <div class="card">
 <div class="card__image-container">
 <img class="card__image"  src='https://www.thebalancesmb.com/thmb/E6hp3YFsPw8mCK_39bw94CxE4Vk=/3456x3456/smart/filters:no_upscale()/asian-businesswoman-leading-meeting-at-boardroom-table-504987926-5ad21419c5542e0036d7003e.jpg' alt=""/>
@@ -23,22 +50,4 @@ const Card = (props)=> {
     <Button type="primary">See More</Button>
 </div>
 </div>
-</div>
-    )
-
-}
-
-export default Card;
-
-///
-///<div>
-          //  <img src={image} alt=""/>
-            //<p>{title}</p>
-            //<p>{category}</p>
-            //<p>{date}</p>
-            //<Button type="primary">Primary</Button>
-       // </div>
-
-// This code to see the final result of the card 
-
-
+</div>*/
