496aa24719aaee1d6ab0af4af31e14f1980c9b6b
rawan <rawan.designer@hotmail.com>
2020-04-05 14:48:17 +0300

    Adding the style of the card component and edit it Relates #13

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -7,18 +7,13 @@ import Card from '../common/Card'
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <h1>Events Booker</h1>
       <Card />
-||||||| merged common ancestors
-      <h1>Events Booker</h1>
-=======
       <Router>
       <Switch>
 
       </Switch>
       </Router>
->>>>>>> 22748eb50be7ef9ea21ab2f671a99355af7e82d4
     </div>
   );
 }
--- client/src/components/common/Card/index.js
+++ client/src/components/common/Card/index.js
@@ -1,8 +1,7 @@
 //react function component 
 import React from 'react';
-//import '../Card/style.css'
+import './style.css'
 import { Button } from "antd";
-//import 'antd/dist/antd.css';
 
 
 
@@ -11,15 +10,19 @@ const Card = (props)=> {
     console.log(props);
 
     return(
-        <div>
-
-        <img src='https://www.thebalancesmb.com/thmb/E6hp3YFsPw8mCK_39bw94CxE4Vk=/3456x3456/smart/filters:no_upscale()/asian-businesswoman-leading-meeting-at-boardroom-table-504987926-5ad21419c5542e0036d7003e.jpg' alt=""/>
-        <p>Event</p>
-        <p>coding</p>
-        <p>12/12</p>
-        
-        <div>
-        <Button type="primary">Primary</Button>
+        <div class="container">
+        <div class="card">
+            <div class="card__image-container">
+            <img class="card__image"  src={image} alt=""/>
+            </div>
+            <div class="card__content">
+            <p class='card__p'><b>Event Title :</b> {title}</p>
+            <p class='card__p'><b>Event category :</b> {category}</p>
+            <p class='card__p'><b>Event Date :</b> {date}</p>
+            </div>
+            <div class='card__button'>
+                <Button type="primary">See More</Button>
+            </div>
         </div>
     </div>
     );
@@ -36,3 +39,20 @@ export default Card;
             //<p>{date}</p>
             //<Button type="primary">Primary</Button>
        // </div>
+
+// This code to see the final result of the card 
+/* <div class="container">
+<div class="card">
+<div class="card__image-container">
+<img class="card__image"  src='https://www.thebalancesmb.com/thmb/E6hp3YFsPw8mCK_39bw94CxE4Vk=/3456x3456/smart/filters:no_upscale()/asian-businesswoman-leading-meeting-at-boardroom-table-504987926-5ad21419c5542e0036d7003e.jpg' alt=""/>
+</div>
+<div class="card__content">
+<p class='card__p'><b>Event Title :</b> Coding For Everyone</p>
+<p class='card__p'><b>Event category :</b> Coding</p>
+<p class='card__p'><b>Event Date :</b> 12/12/2020</p>
+</div>
+<div class='card__button'>
+    <Button type="primary">See More</Button>
+</div>
+</div>
+</div>*/
