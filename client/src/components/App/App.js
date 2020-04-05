8b1cac543db7efb01557b0df1490b1bd27fdbf41
rawan <rawan.designer@hotmail.com>
2020-04-05 21:54:17 +0300

    Adding the footer component Relates #9

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router,Route, Switch } from 'react-router-dom';
 import { Button } from 'antd';
+import Footer from '../common/Footer'
+
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Footer />
       <button>See More</button>
 
       <Router>
---
+++ client/src/components/common/Footer/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+
+const Footer= () => {
+  return (
+      <div>
+          <div>
+            <h5 className="title">Footer Content</h5>
+            <p>
+             Here you can use rows and columns here to organize your footer
+             content.
+            </p> 
+          </div>
+          
+          <div>
+
+            <h5 className="title">Links</h5>
+            <ul>
+                <li className="list-unstyled">
+                    <a href="#!">Link 1</a>
+                </li>
+                <li className="list-unstyled">
+                    <a href="#!">Link 2</a>
+                </li>
+                <li className="list-unstyled">
+                    <a href="#!">Link 3</a>
+                </li>
+                <li className="list-unstyled">
+                    <a href="#!">Link 4</a>
+                </li>
+            </ul>
+          </div>
+
+          <div>
+            <p>&copy; {new Date().getFullYear()} Copyright: <a href="https://www.mdbootstrap.com"> MDBootstrap.com </a></p> 
+          </div>
+      </div>
+  )
+}
+
+export default Footer;
+
+
+
+<div>
+&copy; {new Date().getFullYear()} Copyright: <a href="https://www.mdbootstrap.com"> MDBootstrap.com </a> 
+</div>
