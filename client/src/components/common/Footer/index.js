3519974493e4e5b2c0853c9f2b41fdae8e165e3b
rawan <rawan.designer@hotmail.com>
2020-04-06 22:22:49 +0300

    Edit the style of the footer

--- client/src/components/common/Footer/index.js
+++ client/src/components/common/Footer/index.js
@@ -6,7 +6,7 @@ const Footer= () => {
   return (
       <div>
          
-          <footer class="footer-distributed">
+          <footer class="footer-container">
 
 			<div class="footer-left">
 				<h3>EVENTS<span>BOOKER</span></h3>
@@ -14,7 +14,7 @@ const Footer= () => {
 
 			<div class="footer-center">
 
-				<div>
+				<div class="footer-map">
 					<i class="fa fa-map-marker"></i>
 					<p>UNWRA HQ Square, Almotaz 3 Building, Mezzanine level</p>
 				</div>
@@ -33,7 +33,7 @@ const Footer= () => {
 
 			<div class="footer-right">
 
-				<p class="footer-company-about">
+				<p class="footer-app-about">
 					<span>About Our App:</span>
 					Our app is a tool to help GSG in organizing the registration for the events that they held there. 
 
@@ -41,10 +41,10 @@ const Footer= () => {
 
 				<div class="footer-icons">
 
-					<a href="#"><i class="fa fa-facebook"></i></a>
-					<a href="#"><i class="fa fa-twitter"></i></a>
-					<a href="#"><i class="fab fa-youtube"></i></a>
-					<a href="#"><i class="fab fa-instagram"></i></a>
+					<a href="https://www.facebook.com/GazaSkyGeeks/"><i class="fa fa-facebook"></i></a>
+					<a href="https://twitter.com/gazaskygeeks"><i class="fa fa-twitter"></i></a>
+					<a href="https://www.youtube.com/gazaskygeeks"><i class="fab fa-youtube"></i></a>
+					<a href="https://instagram.com/gazaskygeeks"><i class="fab fa-instagram"></i></a>
 
 				</div>
 
