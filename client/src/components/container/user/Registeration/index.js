98c7f81285c32f9c18aef5a8595863a91d956308
mariamisa <maryaam1998@hotmail.com>
2020-04-06 18:54:30 +0300

    fix req change relates #14

--- client/src/components/container/user/Registeration/index.js
+++ client/src/components/container/user/Registeration/index.js
@@ -1,27 +1,29 @@
 import React ,{Component}from 'react';
 import axios from 'axios';
-import './style.css'
 import { Button ,Form,Input} from 'antd';
 import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
 
+import './style.css'
 
 class RegisterUser extends Component {
   state = {
-    responseMsg:'',
-    redirect: false
+    success : false,
+    redirect: false,
+    message:''
   }
-    onFinishFailed = ({ errorFields }) => {
-      Form.scrollToField(errorFields[0].name);
-    };  
-    onFinish=value=>{
-      const {firstName,lastName,location,email}=value
-      const { match: { params } } = this.props;
-      console.log(this.props);
-      const {mobileNo,eventCode}=params
+    
+    
+    onFinish=({ firstName, lastName, location, email })=>{
+      const {
+        match: { params : {mobileNo, eventCode} },
+      } = this.props;
       axios.post(`/api/v1/register`,
       {firstName,lastName,location,email,mobile:mobileNo,eventCode }).then(res => {
-        this.setState({responseMsg:res.data.msg})
+        this.setState({ redirect: true, success : true })
+      }).catch(err=>{
+        console.log(err.Error);
+        this.setState({message:"error on registration"})
       })
     }
 
@@ -37,13 +39,16 @@ class RegisterUser extends Component {
     }
 
   render() {
+      const {
+        match: { params : {eventCode} },} = this.props;
+        const { redirect , success,message } = this.state;
       return (
         <div className="main">
           <div className="s1">
             <img alt="logo" src="https://raw.githubusercontent.com/GSG-G8/ca-wiki/master/client/public/favicon.ico"/>
             <h1><span className="blue">welcome to</span> GSG event app</h1>
             <h2>Register your data to show the available events and choose your favorite</h2>
-            <Form onFinish={this.onFinish} onFinishFailed={this.onFinishFailed}>
+            <Form onFinish={this.onFinish} >
 
               <Form.Item name="firstName" 
               rules={[{ required: true, message: 'Please input your firstName!'}]}>
@@ -64,15 +69,19 @@ class RegisterUser extends Component {
               rules={[{ required: true, message: 'Please input your location!'}]}>
                 <Input name="location" placeholder="location" />
               </Form.Item>
+              {message&&<div className="errorHandel">{message}</div>}
               <Button htmlType="submit" type="primary"> Submit </Button>
-              {this.renderRedirect()}
-              <Button type="danger" onClick={this.setRedirect}> Exit </Button>
+              
+              {redirect && (<Redirect to={{pathname: `/event/${eventCode}`
+              ,state: { success }, }} />)}
+              <Button type="danger" onClick={() => this.setState({ redirect: true })}> Exit </Button>
             </Form>
           </div>
           <div className="s2">
             <img alt="img" src="https://www.thebalancesmb.com/thmb/E6hp3YFsPw8mCK_39bw94CxE4Vk=/3456x3456/smart/filters:no_upscale()/asian-businesswoman-leading-meeting-at-boardroom-table-504987926-5ad21419c5542e0036d7003e.jpg" />
           </div>
         </div>
+        
       )
   }
 }
