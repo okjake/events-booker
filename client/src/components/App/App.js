682a77f96f2cb746aefa4681d4c1246e09549ec6
rawan <rawan.designer@hotmail.com>
2020-04-07 01:20:02 +0300

    Adding the structure of the footer copyright Relates #9

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router,Route, Switch } from 'react-router-dom';
-<<<<<<< HEAD
 import Footer from '../common/Footer'
 
-||||||| merged common ancestors
-import { Button } from 'antd';
-=======
->>>>>>> c999a48132443012a135f02c9e7b6c0b4b7c017e
 
 import './App.css';
 
