60bf77a5a529b442884dda5739ca0845dd0db438
mariamisa <maryaam1998@hotmail.com>
2020-04-07 16:54:02 +0300

    fix style relates #14

--- client/src/components/container/user/Registeration/index.js
+++ client/src/components/container/user/Registeration/index.js
@@ -1,6 +1,6 @@
 import React ,{Component}from 'react';
 import axios from 'axios';
-import { Button ,Form,Input,Alert} from 'antd';
+import { Button ,Form,Input,Alert, } from 'antd';
 import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
 import { Spin } from 'antd';
@@ -13,20 +13,28 @@ class RegisterUser extends Component {
     success : false,
     redirect: false,
     successMsg:'',
-    message:'',
-    isLoade:true
+    emailMsg:'',
+    isLoade:false,
+    serverError:''
   }
      
     onFinish=({ firstName, lastName, location, email })=>{
-      this.setState({isLoade:false})
+      this.setState({isLoade:true})
       const {
         match: { params : {mobileNo, eventCode} },
       } = this.props;
       axios.post(`/api/v1/register`,
       {firstName,lastName,location,email,mobile:mobileNo,eventCode }).then(({data}) => {
-        this.setState({ redirect: true, success : true,successMsg:data.msg,isLoade:true})
+        console.log(data);
+        if(data.status===200){
+        this.setState({successMsg:data.message ,success : true,})
+      }
+        else{
+          this.setState({emailMsg:data.message})
+        }
+        this.setState({ redirect: true,isLoade:false})
       }).catch(err=>{
-        this.setState({message:"email exist !!",isLoade:true})
+        this.setState({emailMsg:"email exists !!",isLoade:false})
       })
     }
 
@@ -44,7 +52,7 @@ class RegisterUser extends Component {
   render() {
       const {
         match: { params : {eventCode} },} = this.props;
-        const { redirect , success,message ,successMsg ,isLoade} = this.state;
+        const { redirect , success,emailMsg ,successMsg ,isLoade} = this.state;
 
         const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
 
@@ -70,24 +78,26 @@ class RegisterUser extends Component {
               rules={[{ required: true, message: 'Please input your email!'}]}>
                 <Input name="email" placeholder="email" />
               </Form.Item>
-              {message&& <Alert message={message} type="error" />}
+              {emailMsg&& <Alert message={emailMsg} type="error" />}
 
 
               <Form.Item name="location" 
               rules={[{ required: true, message: 'Please input your location!'}]}>
                 <Input name="location" placeholder="location" />
               </Form.Item>
+              {success && (<Alert message={successMsg} type="success" showIcon />)}
 
               {successMsg  && (<Redirect to={{pathname: `/event/${eventCode}`
               ,state: { success,successMsg }, }} />)}
 
               <Button htmlType="submit" 
               type="primary" 
-              >{!isLoade&& (<Spin indicator={antIcon} />)}
+              >{isLoade&& (<Spin indicator={antIcon} />)}
                Submit </Button>
               
               {redirect && (<Redirect to={{pathname: `/event/${eventCode}`
-              ,state: { success,message }, }} />)}
+              ,state: { success,successMsg }, }} />)}
+
               <Button type="danger" 
               onClick={() => this.setState({ redirect: true })}> Exit </Button>
             </Form>
