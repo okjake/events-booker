5807ccc747fd23b692ec7ffa75e698ca671d568f
rawan <rawan.designer@hotmail.com>
2020-04-08 19:56:53 +0300

    Adding the loginAdmin component

--- client/src/components/container/admin/LogInPage/index.js
+++ client/src/components/container/admin/LogInPage/index.js
@@ -1,2 +1,19 @@
 // Admin login
-import React from 'react'
+import React,{component} from 'react';
+import axios from 'axios';
+import {Form, Button, Input} from 'antd';
+
+import './style.css';
+
+class AdminLogIn extends component{
+    state = {
+        username:'',
+        password: ''
+    }
+}
+
+
+
+render(){
+
+}
