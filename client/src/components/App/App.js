7445fd6db8654bf8171a6d03a40d6132a137cc14
mariamisa <maryaam1998@hotmail.com>
2020-04-08 23:33:11 +0300

    Dashboard componant relates #31

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch ,Link} from 'react-router-dom';
 import EventPage from '../container/user/EventPage/index';
 import Landing from "../container/user/LandingPage";
-
+import Dashboard from "../container/admin/controlPage/MainPage"
 import "antd/dist/antd.css";
 import './App.css';
 
@@ -10,9 +10,11 @@ const App = () => {
   return (
     <div className="App">
       <Router>
+        <Link to="/admin/dashboard"> dashboard page</Link>
         <Switch>
           <Route exact path='/events/:eventProg/:eventCode' component={EventPage} />
           <Route exact path="/" component={Landing} />
+          <Route exact path="/admin/dashboard" component={Dashboard} />
         </Switch>
       </Router>
     </div>
