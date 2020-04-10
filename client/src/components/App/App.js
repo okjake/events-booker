6f05452fb62f62530ee5f9d5084b6ba479c2370d
mariamisa <maryaam1998@hotmail.com>
2020-04-10 20:49:15 +0300

    usersPage relates #37

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import EventPage from '../container/user/EventPage/index';
-import Landing from "../container/user/LandingPage";
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import EventPage from '../container/user/EventPage';
+import Landing from '../container/user/LandingPage';
 import Register from '../container/user/Registeration'
+import UsersPage from '../container/admin/UsersPage'
 
 import "antd/dist/antd.css";
 import './App.css';
@@ -11,12 +12,13 @@ const App = () => {
   return (
     <div className="App">
       <Router>
+        <Link to="/dashboard/:eventcode/users">users</Link>
         <Switch>
           <Route exact path='/events/:eventProg/:eventCode' component={EventPage} />
           <Route exact path="/" component={Landing} />
           <Route exact path="/register/:eventProg/:eventCode/:mobile" 
-          component={Register}>
-      </Route>
+          component={Register}/>
+          <Route exact path="/dashboard/:eventcode/users" component={UsersPage}/>
         </Switch>
       </Router>
     </div>
---
+++ client/src/components/container/admin/UsersPage/index.js
@@ -0,0 +1,10 @@
+import React,{Component} from "react";
+import "./style.css"
+
+class UsersPage extends Component {
+    render(){
+        return (<div> hello from users page</div>)
+    }
+}
+
+export default UsersPage
