c6501ddb5389a2f9d20332eb7b2465a68174e2cb
mariamisa <maryaam1998@hotmail.com>
2020-04-12 17:11:48 +0300

    edit event user page relates #37

--- client/src/components/container/admin/UsersPage/index.js
+++ client/src/components/container/admin/UsersPage/index.js
@@ -24,13 +24,8 @@ class UsersPage extends Component {
     }
     render(){
         const { error, isLoaded, users } = this.state;
-        if (error) {
-            return <div>Error: {error.message}</div>;
-        } else if (!isLoaded) {
-            return <div><LoadingOutlined /> Loading</div>;
-        } else {
-            return (
-                <div className="tableSection">
+        return(
+            <div className="tableSection">
                 <header className="headerUsers">
                 <div className="content-t">
                 <img alt="logo" src="https://cdn.discordapp.com/attachments/690170174116331638/697432051929972767/GSG_Logo_svg.svg"/>
@@ -40,41 +35,42 @@ class UsersPage extends Component {
                 </div>
                 </header>
                 <h2>All Users On this Event</h2>
-                <div className="content-t">
-                    <ReactHTMLTableToExcel
-                        id="test-table-xls-button"
-                        className="download-table-xls-button"
-                        table="table-to-xls"
-                        filename="eventusers"
-                        sheet="tablexls"
-                        buttonText="export users to xls"/>
-                    <table id="table-to-xls" className="table-user">
-                    <thead>
-                    <tr>
-                        <th>user name</th>
-                        <th>mobile</th>
-                        <th>email</th>
-                        <th>location</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {users.map(({first_name,last_name,location,email,mobile}) => (
-                        <tr key={first_name}>
-                        <td>{first_name+" "+ last_name} </td>
-                        <td>{mobile}</td>
-                        <td>{email}</td>
-                        <td>{location}</td>
+                    {(error) && (<div>Error: {error.message}</div>)}
+                    {(!isLoaded)?<div><LoadingOutlined /> Loading</div>:(
+                        <div className="content-t">
+                        <ReactHTMLTableToExcel
+                            id="test-table-xls-button"
+                            className="download-table-xls-button"
+                            table="table-to-xls"
+                            filename="eventusers"
+                            sheet="tablexls"
+                            buttonText="export users to xls"/>
+                        <table id="table-to-xls" className="table-user">
+                        <thead>
+                        <tr>
+                            <th>user name</th>
+                            <th>mobile</th>
+                            <th>email</th>
+                            <th>location</th>
                         </tr>
-                    ))}
-                    </tbody>
-                    </table>
-                </div>
-
+                        </thead>
+                        <tbody>
+                        {users.map(({first_name,last_name,location,email,mobile}) => (
+                            <tr key={first_name}>
+                            <td>{first_name+" "+ last_name} </td>
+                            <td>{mobile}</td>
+                            <td>{email}</td>
+                            <td>{location}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                        </table>
+                        </div>
+                        )}
+                
             </div>
-            );
-        }
+        )
     }   
-    
 }
 
 
