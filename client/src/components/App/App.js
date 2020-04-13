1db2badb7dab9d69ee08297f47d4a31ee1d575ae
rawan <rawan.designer@hotmail.com>
2020-04-14 00:39:54 +0300

    Solve the conflict

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,26 +1,12 @@
-<<<<<<< HEAD
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import EventPage from '../container/user/EventPage/index';
 import Landing from "../container/user/LandingPage";
 import Register from '../container/user/Registeration'
 import AdminLogin from '../container/admin/LogInPage'
-||||||| merged common ancestors
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import EventPage from '../container/user/EventPage/index';
-import Landing from "../container/user/LandingPage";
-import Register from '../container/user/Registeration'
 
-=======
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
->>>>>>> 730399c57d09eb4fa87883a9c873ff143e5774d5
+
 import "antd/dist/antd.css";
-
-import EventPage from "../container/user/EventPage";
-import Landing from "../container/user/LandingPage";
-import Register from "../container/user/Registeration";
 import "./App.css";
 
 const App = () => {
@@ -28,19 +14,7 @@ const App = () => {
     <div className="App">
       <Router>
         <Switch>
-<<<<<<< HEAD
-          <Route exact path='/events/:eventProg/:eventCode' component={EventPage} />
-          <Route exact path='/' component={Landing} />
-          <Route exact path="/register/:eventProg/:eventCode/:mobile" 
-          component={Register} />
-            <Route exact path='/admin' component={AdminLogin}/>
-||||||| merged common ancestors
-          <Route exact path='/events/:eventProg/:eventCode' component={EventPage} />
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/register/:eventProg/:eventCode/:mobile" 
-          component={Register}>
-      </Route>
-=======
+
           <Route
             exact
             path="/events/:eventProg/:eventCode"
@@ -52,7 +26,9 @@ const App = () => {
             path="/register/:eventProg/:eventCode/:mobile"
             component={Register}
           />
->>>>>>> 730399c57d09eb4fa87883a9c873ff143e5774d5
+
+           <Route exact path='/admin' component={AdminLogin}/>
+
         </Switch>
       </Router>
     </div>
