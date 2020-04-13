0ea81d976d6de6a0cedec9a23910a9c5fc29a573
MohammedIrheem <mohammedirheem96@gmail.com>
2020-04-13 22:28:06 +0300

    style the portal login page

--- client/src/components/container/portal/LogInPage/index.js
+++ client/src/components/container/portal/LogInPage/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Form, Input, Button, Spin, Alert } from 'antd';
+import { Form, Input, Button, Spin, Alert, message } from 'antd';
 import axios from 'axios';
 
 import './style.css'
@@ -9,20 +9,25 @@ export class PortalLogin extends Component {
     picCode: '',
     serverError: '',
     msg: '',
-    error: false
+    error: false,
   }
+
+  handleChange = ({target:{value}}) => {
+    this.setState({ pinCode: value, error: false });
+  };
+
   onFinish = ({ pinCode }) => {
-    this.setState({ isLoade: true, })
-    axios.post('/api/v1/portal/login', { pinCode }).then(({ data }) => {
+    axios.post('/api/v1/portal/login', { pinCode}).then(({ data }) => {
       if (data.status === 301) {
         this.props.history.push('/portal/front');
+        message.success(data.msg, 10);
       } else if (data.status === 400) {
         this.setState({
           error: true,
           msg: data.msg,
           pinCode: '',
         })
-    } else if (data.status === 401){
+      } else if (data.status === 401) {
         this.setState({
           error: true,
           msg: data.msg,
@@ -30,7 +35,7 @@ export class PortalLogin extends Component {
         })
       }
       this.setState({ isLoade: false })
-  }).catch(() => {
+    }).catch(() => {
       this.setState({ serverError: "Internal server error !!", isLoade: false })
     })
   };
@@ -39,33 +44,35 @@ export class PortalLogin extends Component {
     const { isLoade, error, msg } = this.state;
     return (
       <div className='portal-contant'>
-        <h1>Welcome to GSG Events portal page</h1>
+        <h1 className='title'>Welcome to <span>GSG Events portal login page</span></h1>
         <Form
+          className='main-form'
           onFinish={this.onFinish}
         >
           <Form.Item
-            label="Pin Code"
+            className='input-field'
             name="pinCode"
             rules={[{ message: 'Please input your pin-code!' }]}
           >
-            <Input.Password placeholder='Enter your pin code' />
+            <Input.Password placeholder='Enter your pin code' onChange={this.handleChange}/>
           </Form.Item>
-          {isLoade && (<Spin />)}
           <Form.Item >
             <Button type="primary" htmlType="submit">
               Login
             </Button>
           </Form.Item>
-          {
-            error ?
-              <Alert
-                style={{ width: '75%' }}
-                message={msg}
-                type="error"
-                showIcon />
-              : null
-          }
+          {isLoade && (<Spin />)}
         </Form>
+        {
+          error ?
+            <Alert
+              
+              className='alert'
+              message={msg}
+              type="error"
+              showIcon />
+            : null
+        }
       </div>
     );
   }
