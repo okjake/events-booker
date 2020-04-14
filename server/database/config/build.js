2a7039cb535b1e2e2f219f039cdca7329137fcbc
AlaaTaima <alaa.ktaima@gmail.com>
2020-04-14 23:27:07 +0300

    delete fake data file

--- server/database/config/build.js
+++ server/database/config/build.js
@@ -5,8 +5,7 @@ const connection = require('./connection');
 
 const dbBuild = () => {
   const sqlBuild = readFileSync(join(__dirname, 'build.sql')).toString();
-  const sqlFake = readFileSync(join(__dirname, 'fakeData.sql')).toString();
-  return connection.query(sqlBuild + sqlFake);
+  return connection.query(sqlBuild);
 };
 
 module.exports = { dbBuild };
