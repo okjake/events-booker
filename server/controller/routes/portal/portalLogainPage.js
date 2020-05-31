7f8e558778899bbd2007f418b0df41145a7c9575
mariamisa <maryaam1998@hotmail.com>
2020-05-31 22:45:32 +0300

    validation relates #144

--- server/controller/routes/portal/portalLogainPage.js
+++ server/controller/routes/portal/portalLogainPage.js
@@ -3,17 +3,19 @@ const jwt = require('jsonwebtoken');
 
 const getAdmin = require('../../../database/queries/getAdmin');
 
-const pinCodeValidation = (req, res, next) => {
+const pinCodeValidation = async (req, res, next) => {
   const schema = yup.object().shape({
     pinCode: yup.string().required(),
   });
-  schema.validate(req.body, { abortEarly: false })
-    .then(() => {
-      next();
-    })
-    .catch((err) => {
+  try {
+    await schema.validate(req.body, { abortEarly: false });
+    next();
+  } catch (err) {
+    if (err.name === 'ValidationError') {
       res.status(400).json({ msg: err.message });
-    });
+    }
+    return next(err);
+  }
 };
 
 const getHashedPinCode = (req, res, next) => {
