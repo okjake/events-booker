cd71aeb394b8ce82d0ec115c1a1b53c704d3e9e4
mariamisa <maryaam1998@hotmail.com>
2020-05-31 22:29:56 +0300

    check pin code relates #144

--- server/middleware/checkPinCode.js
+++ server/middleware/checkPinCode.js
@@ -1,21 +1,21 @@
 const bcrypt = require('bcrypt');
 
-// refactor promise to await async
-const checkPinCode = (req, res, next) => {
+const checkPinCode = async (req, res, next) => {
   const hashPinCode = req.pinCode;
   const { pinCode } = req.body;
-  bcrypt.compare(pinCode, hashPinCode).then((valid) => {
+  try {
+    const valid = await bcrypt.compare(pinCode, hashPinCode);
     if (!valid) {
       const err = new Error();
-      err.msg = 'incorect pin code';
+      err.msg = 'incorrect pin code';
       err.status = 403;
       throw err;
     } else {
       next();
     }
-  }).catch((err) => {
+  } catch (err) {
     next(err);
-  });
+  }
 };
 
 module.exports = checkPinCode;
