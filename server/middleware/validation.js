91b7411f261bdb1ce7fc08f55c60cacad5502265
MohammedIrheem <mohammedirheem96@gmail.com>
2020-06-01 21:24:36 +0300

    change loginValidation function from promis to try-catch

--- server/middleware/validation.js
+++ server/middleware/validation.js
@@ -15,19 +15,20 @@ const validationCancelReg = (req, res, next) => {
     });
 };
 
-const loginValidation = (req, res, next) => {
+const loginValidation = async (req, res, next) => {
   const schema = object().shape({
     email: string().email().required(),
     password: string().min(8).required(),
   });
-  schema
-    .validate(req.body, { abortEarly: false })
-    .then(() => {
-      next();
-    })
-    .catch((err) => {
+  try {
+    await schema.validate(req.body, { abortEarly: false });
+    next();
+  } catch (error) {
+    if (error.name === 'ValidationError') {
       res.status(400).json({ msg: 'Invalid Inputs!' });
-    });
+    } else {
+      next(error);
+    }
+  }
 };
-
 module.exports = { validationCancelReg, loginValidation };
