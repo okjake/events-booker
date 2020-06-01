8091b3980be14dbfc9160f37cdb9726b00794d30
rawan <rawan.designer@hotmail.com>
2020-06-01 18:34:15 +0300

    Refactor the admin route Relates #144

--- server/controller/routes/admin/getAdmin.js
+++ server/controller/routes/admin/getAdmin.js
@@ -1,8 +1,11 @@
 const { getAdminData } = require('../../../database/queries/admin');
 
+//Convert the promise to async and await 
 const getAdmin = (req, res, next) => {
   getAdminData()
     .then(({ rows }) => res.json(rows))
     .catch(next);
+}; 
 };
+
 module.exports = getAdmin;
