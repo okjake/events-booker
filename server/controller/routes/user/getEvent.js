1995d545c411eb9d982b443f52a0acbe07f2649e
rawan <rawan.designer@hotmail.com>
2020-06-01 23:44:45 +0300

    Convert the getEventByCode from promise to async and await Relates #144

--- server/controller/routes/user/getEvent.js
+++ server/controller/routes/user/getEvent.js
@@ -1,10 +1,13 @@
 const { getEventDetails } = require('../../../database/queries/events/index');
 
-const getEventByCode = (req, res, next) => {
-  const { eventcode } = req.params;
-  getEventDetails(eventcode)
-    .then(({ rows }) => res.json(rows[0]))
-    .catch(next);
+const getEventByCode = async (req, res, next) => {
+  try {
+    const { eventcode } = req.params;
+    const { rows } = await getEventDetails(eventcode);
+    res.json(rows[0]);
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports = getEventByCode;
