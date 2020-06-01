58badfd18ef7e22b2dd37ee0773be4b379bb3321
AhmedSafi97 <ahmadhsafi1997@gmail.com>
2020-06-01 10:25:07 +0300

    change eslint config file extension to js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,28 @@
- module.exports = {
-  "env": {
-    "browser": true,
-    "commonjs": true,
-    "es6": true,
-    "node": true
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
   },
-  "extends": ["airbnb-base", "prettier"],
-  "globals": {
-    "Atomics": "readonly",
-    "SharedArrayBuffer": "readonly"
+  extends: ["airbnb-base", "prettier"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
-  "parserOptions": {
-    "ecmaVersion": 2018
+  parserOptions: {
+    ecmaVersion: 2018,
   },
-  "rules": {
+  rules: {
     "no-unused-vars": "off",
     "arrow-body-style": ["error", "as-needed"],
     "prettier/prettier": [
       "error",
       {
-        "trailingComma": "es5",
-        "singleQuote": true
-      }
-    ]
+        trailingComma: "es5",
+        singleQuote: true,
+      },
+    ],
   },
-  "plugins": ["prettier"]
-}
+  plugins: ["prettier"],
+};
