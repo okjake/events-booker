1130e90000f3530c45e98db2e1569fc22b2a56cc
rawan <rawan.designer@hotmail.com>
2020-06-01 18:38:03 +0300

    Convert the getAdmin from promise to async and await

--- server/controller/routes/admin/getAdmin.js
+++ server/controller/routes/admin/getAdmin.js
@@ -1,11 +1,11 @@
 const { getAdminData } = require('../../../database/queries/admin');
 
-//Convert the promise to async and await 
-const getAdmin = (req, res, next) => {
-  getAdminData()
-    .then(({ rows }) => res.json(rows))
-    .catch(next);
-}; 
-};
+const getAdmin = async (req, res, next) => {
+  try {
+    const { rows } = await getAdminData();
+    res.json(rows);
+  } catch (err) {
+    next(err);
+  }
 
 module.exports = getAdmin;
