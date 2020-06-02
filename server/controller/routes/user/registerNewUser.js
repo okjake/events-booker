06afb3089bd83c6b514f70a8de65f78a7fd63cea
AhmedSafi97 <ahmadhsafi1997@gmail.com>
2020-06-02 11:34:05 +0300

    right tests for /register route

--- server/controller/routes/user/registerNewUser.js
+++ server/controller/routes/user/registerNewUser.js
@@ -4,12 +4,14 @@ const { getUser } = require('../../../database/queries/users');
 const { newUser } = require('../../../database/queries/users');
 const { checkUserExist } = require('../../../database/queries/users');
 
+const mobileRegExp = /^(\+?\d{0,4})?\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{4}\)?)?$/;
+
 const schema = yup.object().shape({
   firstName: yup.string().required().max(10),
   lastName: yup.string().required().max(10),
-  email: yup.string().email().required(),
-  mobile: yup.number().positive().integer().required(),
-  location: yup.string().required(),
+  email: yup.string().email().required().max(50),
+  mobile: yup.string().required().max(20).matches(mobileRegExp),
+  location: yup.string().required().max(50),
 });
 
 const signUpUser = async (req, res, next) => {
@@ -24,9 +26,11 @@ const signUpUser = async (req, res, next) => {
     await newUser(req.body);
     return res.sendStatus(201);
   } catch (err) {
-    if (err.name === 'ValidationError')
-      return res.status(400).json({ msg: err.message });
-
+    if (err.name === 'ValidationError') {
+      if (err.errors[0].includes('mobile must match the following'))
+        return res.status(400).json({ msg: 'invalid mobile number' });
+      return res.status(400).json({ msg: err.errors[0] });
+    }
     return next(err);
   }
 };
