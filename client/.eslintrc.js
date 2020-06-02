3c8ba4fae498b6c132921d1037e738f339914086
rawan <rawan.designer@hotmail.com>
2020-06-02 15:30:53 +0300

    Edit the eslint rule

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -42,5 +42,14 @@ module.exports = {
       }
     ]
   },
-  plugins: ['react', 'prettier']
+  plugins: ['react', 'prettier'],
+
+  "camelcase": [0], //no-camelCase
+    "react/prop-types": [
+      0
+    ], //no propTypes
+    'no-nested-ternary': [0, {allowParensWrapped: true}], // nested ternary
+
+    "react/jsx-props-no-spreading": 0,
+
 };
