f5542bd25569ce0d920570d848107d7b98bf2d60
rawan <rawan.designer@hotmail.com>
2020-06-07 21:48:02 +0300

    Convert the test of the event route from promise to async/await

--- __tests__/routes/event.test.js
+++ __tests__/routes/event.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable jest/no-commented-out-tests */
 require('env2')('./config.env');
 const request = require('supertest');
 
@@ -11,111 +12,93 @@ describe('get request to /api/v1/event', () => {
   beforeAll(() => dbBuild());
   afterAll(() => connection.end());
 
-  it('respond with json containing a list of all events', () =>
-    new Promise((done) => {
-      expect.assertions(8);
-      request(app)
-        .get('/api/v1/event')
-        .set('Accept', 'application/json')
-        .set('Cookie', [`token=${TOKEN}`])
-        .expect('Content-Type', /json/)
-        .expect(200)
-        .end((err, res) => {
-          if (err) throw err;
-          const data = res.body[0];
-          expect(data.title).toBe('Express');
-          expect(data.event_code).toBe(303);
-          expect(data.category).toBe('Code Academy');
-          expect(data.details).toBe('lorem ipsum');
-          expect(data.image).toBe('https://i.imgur.com/VgTVTNA.jpg');
-          expect(data.date).toBe('2020-05-09T12:00:00.000Z');
-          expect(data.duration).toBe(90);
-          expect(data.count).toBe('0');
-          return done();
-        });
-    }));
+  it('respond with json containing a list of all events', async () => {
+    expect.assertions(1);
+    const res = await request(app)
+      .get('/api/v1/event')
+      .set('Accept', 'application/json')
+      .set('Cookie', [`token=${TOKEN}`])
+      .expect('Content-Type', /json/)
+      .expect(200);
+    expect(res.body[0]).toStrictEqual({
+      title: 'Express',
+      id: 2,
+      event_code: 303,
+      category: 'Code Academy',
+      details: 'lorem ipsum',
+      image: 'https://i.imgur.com/VgTVTNA.jpg',
+      date: '2020-05-09T12:00:00.000Z',
+      duration: 90,
+      count: '0',
+    });
+  });
 
-  it('create a new event', () =>
-    new Promise((done) => {
-      expect.assertions(2);
-      const reqBody = {
-        title: 'How to Think Like a Programmer',
-        eventCode: 700,
-        category: 'Code Academy',
-        details:
-          'This event is for the people who are new to the programming field!!!',
-        image: 'https://i.imgur.com/VgTVTNA.jpg',
-        date: '2020-05-09T12:00:00.000Z',
-        duration: 120,
-      };
-      request(app)
-        .post('/api/v1/event')
-        .send(reqBody)
-        .set('Accept', 'application/json')
-        .set('Cookie', [`token=${TOKEN}`])
-        .expect(200)
-        .end((err, res) => {
-          if (err) throw err;
-          expect(res.statusCode).toBe(200);
-          expect(res.text).toBe(
-            '{"msg":"Event How to Think Like a Programmer has been created successfully"}'
-          );
-          return done();
-        });
-    }));
+  it('create a new event', async () => {
+    expect.assertions(1);
+    const reqBody = {
+      title: 'How to Think Like a Programmer',
+      eventCode: 700,
+      category: 'Code Academy',
+      details:
+        'This event is for the people who are new to the programming field!!!',
+      image: 'https://i.imgur.com/VgTVTNA.jpg',
+      date: '2020-05-09T12:00:00.000Z',
+      duration: 120,
+    };
+    const res = await request(app)
+      .post('/api/v1/event')
+      .send(reqBody)
+      .set('Accept', 'application/json')
+      .set('Cookie', [`token=${TOKEN}`])
+      .expect(200);
+    expect(res.body).toStrictEqual({
+      msg: 'Event How to Think Like a Programmer has been created successfully',
+    });
+  });
 
-  it('return bad request with status 400 if the inputs are invalid', () =>
-    new Promise((done) => {
-      expect.assertions(1);
-      const reqBody = {
-        title: 'How to Refactor Your code Professionally ',
-        event_code: '900',
-        category: 'Code Academy',
-        details:
-          'This event is for the people who are facing problem with Refactoring!!',
-        image: 'https://i.imgur.com/VgTVTNA.jpg',
-        date: '2020-05-09T12:00:00.000Z',
-        duration: 120,
-      };
-      request(app)
-        .post('/api/v1/event')
-        .send(reqBody)
-        .set('Accept', 'application/json')
-        .set('Cookie', [`token=${TOKEN}`])
-        .expect(400)
-        .end((err, res) => {
-          expect(res.text).toBe('{"msg":"invalid inputs"}');
-          if (err) throw err;
-          return done();
-        });
-    }));
+  it('return bad request with status 400 if the inputs are invalid', async () => {
+    expect.assertions(1);
+    const reqBody = {
+      title: 'How to Refactor Your code Professionally ',
+      event_code: '900',
+      category: 'Code Academy',
+      details:
+        'This event is for the people who are facing problem with Refactoring!!',
+      image: 'https://i.imgur.com/VgTVTNA.jpg',
+      date: '2020-05-09T12:00:00.000Z',
+      duration: 120,
+    };
+    const res = await request(app)
+      .post('/api/v1/event')
+      .send(reqBody)
+      .set('Accept', 'application/json')
+      .set('Cookie', [`token=${TOKEN}`])
+      .expect(400);
+    expect(res.body).toStrictEqual({
+      msg: 'invalid inputs',
+    });
+  });
 
-  it('return bad request with status 400 if an event with the code already exist', () =>
-    new Promise((done) => {
-      expect.assertions(1);
-      const reqBody = {
-        title: 'How to Become a professional programmer',
-        eventCode: 700,
-        category: 'Code Academy',
-        details:
-          'This event is for the people who are aiming to become a professional programmer',
-        image: 'https://i.imgur.com/VgTVTNA.jpg',
-        date: '2020-05-09T12:00:00.000Z',
-        duration: 120,
-      };
-      request(app)
-        .post('/api/v1/event')
-        .send(reqBody)
-        .set('Accept', 'application/json')
-        .set('Cookie', [`token=${TOKEN}`])
-        .expect(400)
-        .end((err, res) => {
-          console.log(res);
-          expect(res.text).toBe(
-            '{"msg":"an event with code 700 already exist, try another code"}'
-          );
-          if (err) throw err;
-          return done();
-        });
-    }));
+  it('return bad request with status 400 if an event with the code already exist', async () => {
+    expect.assertions(1);
+    const reqBody = {
+      title: 'How to Become a professional programmer',
+      eventCode: 700,
+      category: 'Code Academy',
+      details:
+        'This event is for the people who are aiming to become a professional programmer',
+      image: 'https://i.imgur.com/VgTVTNA.jpg',
+      date: '2020-05-09T12:00:00.000Z',
+      duration: 120,
+    };
+    const res = await request(app)
+      .post('/api/v1/event')
+      .send(reqBody)
+      .set('Accept', 'application/json')
+      .set('Cookie', [`token=${TOKEN}`])
+      .expect(400);
+    expect(res.body).toStrictEqual({
+      msg: 'an event with code 700 already exist, try another code',
+    });
+  });
 });
