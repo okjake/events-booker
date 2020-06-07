53a2419aeee2cca0a81b9dfa47510291fa91f855
AlaaTaima <alaa.ktaima@gmail.com>
2020-06-07 11:56:10 +0300

    fix back-end route name

--- server/router/index.js
+++ server/router/index.js
@@ -86,7 +86,7 @@ router.patch(
   signAttendance
 );
 
-router.all(['/users', '/event/:eventCode/users', '/event'], protectedRoute);
+router.all(['/users', '/events/:eventCode/users', '/event'], protectedRoute);
 router.get('/users', getUsersData);
 router.get('/events/:eventCode/users', getEventUsers);
 router.patch('/event', deleteEvent);
