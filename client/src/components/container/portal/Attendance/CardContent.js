d689f5e5af14bf0ab79da1b515526867f9db21e5
MohammedIrheem <mohammedirheem96@gmail.com>
2020-06-08 23:41:01 +0300

    add attendance form to the card comp.

--- client/src/components/container/portal/Attendance/CardContent.js
+++ client/src/components/container/portal/Attendance/CardContent.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import moment from 'moment';
 import { withRouter } from 'react-router-dom';
+import AttendanceSubmitForm from './AttendanceSubmitForm';
 
 const CardContent = ({ info }) => {
-  const { category, title, date } = info;
-
+  const { category, title, date, event_code } = info;
   return (
-    <div className="card__content">
-      <h3 className="card__p">
-        <b>{title}</b>
-      </h3>
-      <p className="card__p">{category}</p>
-      <p className="card__p">{moment(date).format('LLLL')}</p>
-    </div>
+    <>
+      <div className="card__content">
+        <h3 className="card__p">
+          <b>{title}</b>
+        </h3>
+        <p className="card__p">{category}</p>
+        <p className="card__p">{moment(date).format('hh:mm a')}</p>
+      </div>
+      <AttendanceSubmitForm event_code={event_code} />
+    </>
   );
 };
 
