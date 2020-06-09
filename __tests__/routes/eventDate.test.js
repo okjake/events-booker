dc240175c50044b29c27c944a806daf593b810f1
AlaaTaima <alaa.ktaima@gmail.com>
2020-06-09 20:42:41 +0300

    fix eventDate route test

--- __tests__/routes/eventDate.test.js
+++ __tests__/routes/eventDate.test.js
@@ -8,10 +8,10 @@ const buildDB = require('../../server/database/config/build');
 const createEvent = require('../../server/database/queries/events/creatEventSql');
 
 describe('get request to /event/date', () => {
-  beforeAll(() => {
+  beforeAll(async () => {
     buildDB();
     const date = moment().format('YYYY-MM-DD h:mm:ss');
-    createEvent({
+    await createEvent({
       title: 'test event',
       eventCode: 505,
       category: 'Code Academy',
@@ -24,15 +24,16 @@ describe('get request to /event/date', () => {
   afterAll(() => connection.end());
 
   it('respond with json containing a list of all events on this day', async () => {
-    expect.assertions(1);
+    expect.assertions(2);
     const res = await request(app)
       .get('/api/v1/event/date')
       .set('Accept', 'application/json')
       .set('Cookie', [`portalToken=${process.env.PORTAL_TOKEN}`])
       .expect(200)
       .expect('Content-Type', /json/);
-    expect(res.body).toStrictEqual({ events: 'no events available at GSG' });
-    console.log(res.body);
+
+    expect(res.body[0].category).toStrictEqual('Code Academy');
+    expect(res.body[0].title).toStrictEqual('test event');
   });
 
   it("respond with un-auth msg if he doesn't login", async () => {
