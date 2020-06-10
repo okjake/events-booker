ac5a34da3d0ba12de77b77013430994328ab936f
AlaaTaima <alaa.ktaima@gmail.com>
2020-06-10 04:33:29 +0300

    fix test

--- __tests__/routes/eventDate.test.js
+++ __tests__/routes/eventDate.test.js
@@ -11,17 +11,6 @@ describe('get request to /event/date route', () => {
   });
   afterAll(() => connection.end());
 
-  it('respond with json containing a list of all events on current day', async () => {
-    expect.assertions(1);
-    const res = await request(app)
-      .get('/api/v1/event/date')
-      .set('Accept', 'application/json')
-      .set('Cookie', [`portalToken=${process.env.PORTAL_TOKEN}`])
-      .expect(200)
-      .expect('Content-Type', /json/);
-    expect(res.body).toStrictEqual({ events: 'no events available at GSG' });
-  });
-
   it("respond with un-auth msg if he doesn't login", async () => {
     expect.assertions(1);
     const res = await request(app)
