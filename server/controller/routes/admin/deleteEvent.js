ebd28b0d0cc649ff43a3aeb3353a5db37f37f810
AlaaTaima <alaa.ktaima@gmail.com>
2020-06-10 06:14:53 +0300

    fix typo

--- server/controller/routes/admin/deleteEvent.js
+++ server/controller/routes/admin/deleteEvent.js
@@ -3,7 +3,7 @@ const { expiredEvent } = require('../../../database/queries/events');
 const deleteEvent = async (req, res, next) => {
   try {
     const data = await expiredEvent(req.body.id);
-    res.json({ sg: 'The event has deleted successfully', data });
+    res.json({ msg: 'The event has deleted successfully', data });
   } catch (error) {
     next(error);
   }
